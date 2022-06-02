ce9ba81205e8ee78234ad9f31b6bdb83364b297c
pedro-oliveira446 <79122660+pedro-oliveira446@users.noreply.github.com>
2022-06-01 23:38:07 -0300

    Add files via upload

--- servidor/servidor.py
+++ servidor/servidor.py
@@ -1,32 +1,62 @@
 import socket
+import time
 
-print("INCICIO");
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-
-s.bind(('', 5555)) # Esta linha define para qual IP e porta o servidor deve aguardar a conexão, que no nosso caso é qualquer IP, por isso o Host é ”
+# 1 Server Setup
+print("Configurando servidor UDP")
 
 ip_local = socket.gethostbyname(socket.gethostname())
 print(f'IP Local: {ip_local}')
 
-while True: # roda enquanto tiver conexão
+HOST = ip_local
+PORT = 5000
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((HOST, PORT))
+sock.settimeout(70.0)
 
-    print("AGUARDANDO CONEXAO"); 
 
-    data = s.recvfrom(2000) # o recv recebe os dados de quem está requisitando
-    # 2000 é o número máx de bytes aceitos
+# 2/5 Esperando receber lista de arquivos do cliente
+print("Recebendo lista de arquivos\n")
+files = []
+while True:
+    data, address = sock.recvfrom(512)
+    if data.decode(errors="ignore") == "stop":
+        break
+    file_name = data.decode()
+    print(f"[{len(files)}] {file_name}")
+    files.append(file_name)
 
-    message = data[0]
+# 6 Enviando ao cliente qual arquivo baixar
+file_choice = int(input("\nQual arquivos receber?"))
+while not (0 <= file_choice < len(files)):
+    print("Opcao invalida!")
+    file_choice = int(input("Qual arquivos receber? "))
 
-    address = data[1]
+sock.sendto(file_choice.to_bytes(4, "little"), address)
 
-    if data == 'dcs':
-        print('Cliente escolheu desconectar o servidor.')
-        break       
-    else:        
-        clientMsg = "Message from Client:{}".format(message)
-        clientIP  = "Client IP Address:{}".format(address)
-    
-    print(clientMsg)
-    print(clientIP)
-s.close()
+
+# 10 Recebendo numero de pacotes
+# Queremos saber em quantos pacotes o arquivo sera mandado
+data = sock.recv(4)
+numero_de_pacotes = int.from_bytes(data, "little")
+
+# 12 Recebendo pacotes
+sock.settimeout(5.0)
+file = open(files[file_choice], "wb")
+pacote_em_kilobytes = 512
+pacote_em_bytes = pacote_em_kilobytes * 8
+
+print(f"Recebendo {numero_de_pacotes} pacotes...")
+start = time.time()
+for i in range(numero_de_pacotes):
+    data = sock.recv(pacote_em_bytes)
+    file.write(data)
+    porcentagem = f"Baixando... {round((100*(i+1))/numero_de_pacotes, 2)}%"
+    # print(porcentagem)
+    print('\r'+porcentagem, end='')
+
+tempo_de_download = round(time.time()-start, 2)
+print(f"\nO download foi completo em {tempo_de_download} sec")
+
+# Limpando buffers e sockets
+file.close()
+sock.close()
